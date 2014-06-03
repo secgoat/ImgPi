9005bbd089abf0d4a83e7915e7d55a15fddda0f3
sec_goat <joshua.tilson@yahoo.com>
2014-06-03 11:27:45 -0400

    Timer work preliminary complete. I have a pygame timer that will help keep track of amount of milliseconds past.
    I have my basic 5 images changing every 5 seconds. Need to add some
    randimization to the times as well as clean up the code and start putting
    things into classes.

--- ImgPi/DisplayWindow.py
+++ ImgPi/DisplayWindow.py
@@ -1,5 +1,6 @@
 import os
 import pygame
+import ImgPiTimer
 
 
 pygame.init()
@@ -12,9 +13,12 @@ WIDTH = vidInfo.current_w
 HEIGHT = vidInfo.current_h
 '''
 
+#time = pygame.time.Clock()
+timer = ImgPiTimer.ImgPiTimer()
 screen = pygame.display.set_mode((640,400))
 running = True
 
+font  = pygame.font.Font('./font/Alpaca54.ttf', 20)
 dirs = os.getcwd()
 
 
@@ -27,19 +31,52 @@ images = os.listdir('./images/')
 #    pyImageRect = pyImages[count].get_rect()
 #    count += 1
 #    print(i)
+imgs = []  # ctreate a blank list, we will store the pygame image renders in here to display later
+for img in images:
+    pyImg = pygame.image.load(os.path.join('images', img))
+    imgs.append(pyImg)
+
+screen.fill((0,0,0))
+imgCounter = 0
 
 while running:
     event = pygame.event.poll()
     if event.type == pygame.QUIT:
         running = False
 
-    screen.fill((0,0,0))
-    for img in images:
+
+
+    #timer.updateTimes()
+    elapsedTime = timer.checkAmountTime()
+    delayTime = timer.convertSecToMs(5)
+    print(str(timer.currentTime))
+    timeBlit = font.render("Current Time: " + str(timer.currentTime / 1000) + " Seconds",1,(255,255,255))
+    pastBlit = font.render("past Time: " + str(timer.previousTime / 1000),1,(255,255,255))
+
+
+    if imgCounter > 4:
+        imgCounter = 0
+
+    if elapsedTime > delayTime:
+        timer.previousTime = timer.currentTime
+        screen.fill((0,0,0))
+        pyImgRect = imgs[imgCounter].get_rect()
+        screen.blit(imgs[imgCounter], pyImgRect)
+        screen.blit(timeBlit, (0,0))
+        screen.blit(pastBlit, (400,0))
+        imgCounter += 1
+    #time = pygame.time.get_ticks()
+
+
+
+
+    pygame.display.flip()
+    #timer.clock.tick(50)
+
+'''  for img in images:
         pyImg = pygame.image.load(os.path.join('images', img))
         pyImgRect = pyImg.get_rect()
         screen.blit(pyImg, pyImgRect)
         pygame.display.flip()
-
-for x in range(0,10):
-    print(x)
+'''
 
---
+++ ImgPi/ImgPiTimer.py
@@ -0,0 +1,30 @@
+import pygame.time
+
+
+class ImgPiTimer:
+    """
+
+    """
+
+    def __init__(self):
+        self.clock = pygame.time.Clock()  # start the clock object to keep track of time
+        self.currentTime = pygame.time.get_ticks()  # this and previousTime will be used to keep track of how much time has passed
+        self.previousTime = pygame.time.get_ticks() #init both to current time as we haven't made nay changes ot time yet
+
+    def convertMsToSec(self, milliseconds):
+        seconds = milliseconds / 1000
+        return seconds
+
+    def convertSecToMs(self, seconds):
+        milliseconds = seconds * 1000
+        return milliseconds
+
+    def updateTimes(self):
+        #self.previousTime = self.currentTime
+        self.currentTime = pygame.time.get_ticks()
+
+    def checkAmountTime(self):
+        self.updateTimes()
+        elapsedTime = self.currentTime - self.previousTime
+        return elapsedTime
+
