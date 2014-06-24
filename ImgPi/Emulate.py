0e94b638e791c37aa6bab3e20767d44a5cd1d0c5
sec_goat <joshua.tilson@yahoo.com>
2014-06-24 16:36:02 -0400

    a couple quick lines for follow up later `

--- ImgPi/Emulate.py
+++ ImgPi/Emulate.py
@@ -1,5 +1,9 @@
 __author__ = 'jtilson'
 
 #http://www.nesfiles.com/ for easy nes downloads nesfiles.com/{}/{}.nes  where {} = game name
+#have to check pages for the ing <a id="ctl00_MainContent_lnkROM" href="/NES/720/720.nes"
+#to find if there is a rom or not.
+
+
 
 
